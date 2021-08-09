3aae9f1d2d80692cabcf0e0c3e4f1d10d181a2b0
kangyunsik <re12io@ajou.ac.kr>
2021-08-09 22:52:54 +0900

    어노테이션으로 컴포넌트들 등록

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     // 이후 concurrent hash map 사용.
---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    회원가입
+    */
+
+    public Long join(Member member) {
+        //같은 이름 중복 허용 X
+        verifyingDuplication(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void verifyingDuplication(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 이름입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Member member) {
+        return memberRepository.findById(member.getId());
+    }
+}
