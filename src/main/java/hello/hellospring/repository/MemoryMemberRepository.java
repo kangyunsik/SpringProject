0cf5d366bf0ff9268c434da64fa0f6f391cc539a
kangyunsik <re12io@ajou.ac.kr>
2021-08-16 21:48:18 +0900

    JdbcMemberRepository 구현 및 설정 변경

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     // 이후 concurrent hash map 사용.
